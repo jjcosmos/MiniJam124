3f03a52c278596e723d88d40c45dc46807bea82b
Jason Jackson <jason.cosmos@gmail.com>
2023-01-21 12:16:31 -0800

    Test slippery sled movement

--- MiniJam124/Assets/Scripts/Game.cs
+++ MiniJam124/Assets/Scripts/Game.cs
@@ -1,21 +1,36 @@
-﻿
-    using System;
-    using UnityEngine;
+﻿using System;
+using UnityEngine;
 
-    public class Game : MonoBehaviour
+public class Game : MonoBehaviour
+{
+    public static Game Singleton;
+    public Player CurrentPlayer;
+    public GameState GameState { get; private set; }
+    [field: SerializeField] public GameSettings Settings { get; private set; }
+
+    private void Awake()
     {
-        public static Game Singleton;
-        public Player CurrentPlayer;
-
-        private void Awake()
+        if (!Singleton)
         {
-            if (!Singleton)
-            {
-                Singleton = this;
-            }
-            else
-            {
-                Destroy(gameObject);
-            }
+            Singleton = this;
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
+
+    private void Start()
+    {
+        GameState = GameState.Racing;
+    }
+}
+
+public enum GameState
+{
+    Menus,
+    AwaitingStart,
+    Racing,
+    Paused,
+    CompletedRace,
+}
