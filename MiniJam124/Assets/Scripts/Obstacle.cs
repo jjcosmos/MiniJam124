5370c89bbdd3eed5f4eb5c9ccbdc87e14a8c260d
Jason Jackson <jason.cosmos@gmail.com>
2023-01-22 16:17:54 -0800

    Some small polish w/ particles

--- MiniJam124/Assets/Scripts/Obstacle.cs
+++ MiniJam124/Assets/Scripts/Obstacle.cs
@@ -4,7 +4,7 @@ public class Obstacle : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.TryGetComponent(out PlayerInventory playerInventory))
+        if (Game.Singleton.GameState == GameState.Racing && collision.collider.TryGetComponent(out PlayerInventory playerInventory))
         {
             playerInventory.Collisionhit();
             //gameObject.SetActive(false);
