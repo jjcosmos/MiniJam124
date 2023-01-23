52c4c4ec5912bc40cd145eb69e32efc0be487f09
Jason Jackson <jason.cosmos@gmail.com>
2023-01-22 16:53:38 -0800

    Added some sfx and menu music

--- MiniJam124/Assets/Scripts/Countdown.cs
+++ MiniJam124/Assets/Scripts/Countdown.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class Countdown : MonoBehaviour
 {
     [SerializeField] private TMP_Text _text;
+    [SerializeField] private AudioSource _src;
+    [SerializeField] private AudioClip _beep;
+    [SerializeField] private AudioClip _go;
     IEnumerator Start()
     {
         Game.Singleton.GameState = GameState.AwaitingStart;
         _text.text = "3";
+        _src.PlayOneShot(_beep);
         yield return new WaitForSeconds(1f);
         _text.text = "2";
+        _src.PlayOneShot(_beep);
         yield return new WaitForSeconds(1f);
         _text.text = "1";
+        _src.PlayOneShot(_beep);
         yield return new WaitForSeconds(1f);
         _text.text = "GO!";
-        yield return new WaitForSeconds(1f);
+        _src.PlayOneShot(_go);
         Game.Singleton.StartRace();
+        yield return new WaitForSeconds(1f);
         _text.gameObject.SetActive(false);
     }
 
